d63722e12eb58340a7c1fb4b89250e4b210508f9
bagusistighfar86 <bagusistighfar86@gmail.com>
2022-06-05 03:34:52 +0700

    add finish collider

--- Assets/Scripts/KartCollider.cs
+++ Assets/Scripts/KartCollider.cs
@@ -6,14 +6,24 @@ using UnityEngine;
 public class KartCollider : MonoBehaviour
 {
     public KartAgent kartAgent;
+    public CheckpointManager _checkpointManager;
 
     // Saat mulai berbenturan
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Wall"))
         {
-        Debug.Log("TABRAK");
-        kartAgent.AddReward(-0.1f);
+            Debug.Log("TABRAK");
+            kartAgent.AddReward(-0.1f);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            Debug.Log("Finishhh OYYY");
+            _checkpointManager.ResetCheckpoints();
         }
     }
 
